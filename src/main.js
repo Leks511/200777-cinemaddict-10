55ae790d107cd9503d0047533663e37861ec1b13
Alexander <a.tselkovnikov@encry.ru>
2019-12-11 12:08:41 +0300

    Разбил на компоненты приложение

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {createFilmCardTemplate} from './components/card';
 import {createFilmDetailsTemplate} from './components/details';
 import {createMenuTemplate} from './components/menu';
+import {createFilmsContentTemplate} from './components/films-content';
 import {createShowMoreButtonTemplate} from './components/show-more-button';
 import {createUserRankTemplate} from './components/user-rank';
 
@@ -11,3 +12,20 @@ const render = (container, template, place) => {
 const headerElement = document.querySelector('.header');
 render(headerElement, createUserRankTemplate(), `beforeend`);
 
+const mainElement = document.querySelector(`.main`);
+render(mainElement, createMenuTemplate(), `beforeend`);
+
+render(mainElement, createFilmsContentTemplate(), `beforeend`);
+
+const filmsContentElement = document.querySelector(`.films`);
+
+const mainFilmsContainerElement = filmsContentElement.querySelector(`.films-list .films-list__container`);
+
+render(mainFilmsContainerElement, createFilmCardTemplate(), `beforeend`);
+render(mainFilmsContainerElement, createShowMoreButtonTemplate(), `afterend`);
+
+const topRatedFilmsContainerElement = filmsContentElement.querySelectorAll(`.films-list--extra`)[0].querySelector(`.films-list__container`);
+const MostCommentedFilmsContainerElement = filmsContentElement.querySelectorAll(`.films-list--extra`)[1].querySelector(`.films-list__container`);
+
+render(topRatedFilmsContainerElement, createFilmCardTemplate(), `beforeend`);
+render(MostCommentedFilmsContainerElement, createFilmCardTemplate(), `beforeend`);
