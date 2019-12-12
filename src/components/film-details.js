9f26063bb6eb0eec2aa574ad03bb7ca81999593b
Alexander <a.tselkovnikov@encry.ru>
2019-12-12 12:25:17 +0300

    Не могу оторваться: добавил в film-details пользовательский рейтинг, осталось всё замокать

--- src/components/film-details.js
+++ src/components/film-details.js
@@ -51,7 +51,7 @@ export const createFilmDetailsTemplate = (details) => {
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Country</td>
-                  <td class="film-details__cell">USA</td>
+                  <td class="film-details__cell">${country}</td>
                 </tr>
                 <tr class="film-details__row">
                   <td class="film-details__term">Genres</td>
@@ -63,7 +63,7 @@ export const createFilmDetailsTemplate = (details) => {
               </table>
 
               <p class="film-details__film-description">
-                The film opens following a murder at a cabaret in Mexico City in 1936, and then presents the events leading up to it in flashback. The Great Flamarion (Erich von Stroheim) is an arrogant, friendless, and misogynous marksman who displays his trick gunshot act in the vaudeville circuit. His show features a beautiful assistant, Connie (Mary Beth Hughes) and her drunken husband Al (Dan Duryea), Flamarion's other assistant. Flamarion falls in love with Connie, the movie's femme fatale, and is soon manipulated by her into killing her no good husband during one of their acts.
+                ${description}
               </p>
             </div>
           </div>
@@ -77,6 +77,56 @@ export const createFilmDetailsTemplate = (details) => {
 
             <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite">
             <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
+          </section>
+        </div>
+
+        <div class="form-details__middle-container">
+          <section class="film-details__user-rating-wrap">
+            <div class="film-details__user-rating-controls">
+              <button class="film-details__watched-reset" type="button">Undo</button>
+            </div>
+
+            <div class="film-details__user-score">
+              <div class="film-details__user-rating-poster">
+                <img src="./images/posters/the-great-flamarion.jpg" alt="film-poster" class="film-details__user-rating-img">
+              </div>
+
+              <section class="film-details__user-rating-inner">
+                <h3 class="film-details__user-rating-title">The Great Flamarion</h3>
+
+                <p class="film-details__user-rating-feelings">How you feel it?</p>
+
+                <div class="film-details__user-rating-score">
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="1" id="rating-1">
+                  <label class="film-details__user-rating-label" for="rating-1">1</label>
+
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="2" id="rating-2">
+                  <label class="film-details__user-rating-label" for="rating-2">2</label>
+
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="3" id="rating-3">
+                  <label class="film-details__user-rating-label" for="rating-3">3</label>
+
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="4" id="rating-4">
+                  <label class="film-details__user-rating-label" for="rating-4">4</label>
+
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="5" id="rating-5">
+                  <label class="film-details__user-rating-label" for="rating-5">5</label>
+
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="6" id="rating-6">
+                  <label class="film-details__user-rating-label" for="rating-6">6</label>
+
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="7" id="rating-7">
+                  <label class="film-details__user-rating-label" for="rating-7">7</label>
+
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="8" id="rating-8">
+                  <label class="film-details__user-rating-label" for="rating-8">8</label>
+
+                  <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="9" id="rating-9" checked>
+                  <label class="film-details__user-rating-label" for="rating-9">9</label>
+
+                </div>
+              </section>
+            </div>
           </section>
         </div>
 
