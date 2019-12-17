ba8d03b8a9da2e394142711572e376d8b6d96787
Alexander <a.tselkovnikov@encry.ru>
2019-12-17 12:02:19 +0300

    Переписал на классы top-rated-films

--- src/components/top-rated-films.js
+++ src/components/top-rated-films.js
@@ -1,13 +1,38 @@
-export const createTopRatedFilmsList = (morkup) => {
+import { createElement } from "../util";
+
+export const createTopRatedFilmsListTemplate = (films) => {
 
   return (
     `<section class="films-list--extra">
       <h2 class="films-list__title">Top rated</h2>
 
       <div class="films-list__container">
-        ${morkup}
+        ${films}
       </div>
 
     </section>`
   );
 };
+
+export default class TopRatedFilmsList {
+  constructor(films) {
+    this._films = films;
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createTopRatedFilmsListTemplate(this._films);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
