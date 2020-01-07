181ff2c88e30012592a4cd3050d5566bb7d6f646
Александр <leks511@bk.ru>
2020-01-07 19:31:29 +0300

    Добавил добавку кнопки и убирание её в зависимости от вида сортировки. Фильмы сортируются

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -100,58 +100,6 @@ export default class PageController {
       render(container, this._notFoundFilmsComponent, RenderPosition.BEFOREEND);
     } else {
 
-      // Найдём главный контейнер для фильмов
-      const mainFilmsContainerElement = this._mainFilmsListComponent.getElement().querySelector(`.films-list__container`);
-
-      // Повесим обработчик клика на сортировку
-      this._sortComponent.setChangeSortTypeHandler((sortType) => {
-        let sortedFilms = [];
-
-        switch (sortType) {
-          case SortType.BY_DATE:
-            sortedFilms = films.slice().sort((a, b) => a.year - b.year);
-            break;
-          case SortType.BY_RATING:
-            sortedFilms = films.slice().sort((a, b) => a.rating - b.rating);
-            break;
-          case SortType.DEFAULT:
-            sortedFilms = films.slice(0, showingFilmsCount);
-            break;
-        }
-
-        mainFilmsContainerElement.innerHTML = ``;
-
-        console.log(sortedFilms);
-      });
-
-      // Отображаем основной список и кнопку загрузки
-      render(container, this._mainFilmsListComponent, RenderPosition.BEFOREEND);
-      render(this._mainFilmsListComponent.getElement(), this._showMoreButtonComponent, RenderPosition.BEFOREEND);
-
-
-      // Отобразим доп. секции с фильмами и сразу найдём в них контейнеры для фильмов
-      render(container, this._topRatedFilmsComponent, RenderPosition.BEFOREEND);
-      const topRatedFilmsContainerElement = this._topRatedFilmsComponent.getElement().querySelector(`.films-list__container`);
-
-      render(container, this._mostCommentedFilmsComponent, RenderPosition.BEFOREEND);
-      const mostCommentedFilmsContainerElement = this._mostCommentedFilmsComponent.getElement().querySelector(`.films-list__container`);
-
-      // Отобразим несколько фильмов при старте
-      let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
-      renderFilmCards(mainFilmsContainerElement, films.slice(0, showingFilmsCount));
-
-      // Отрендерим фильмы в доп. секциях по параметрам
-      checkFilmsOnParam(
-          films,
-          FilmParametres.RATING,
-          topRatedFilmsContainerElement);
-
-      checkFilmsOnParam(
-          films,
-          FilmParametres.COMMENTS,
-          mostCommentedFilmsContainerElement);
-
-
       // Функция добавления карточек на доску по нажатию
       const showMoreFilms = () => {
         const prevFilmsCount = showingFilmsCount;
@@ -166,6 +114,80 @@ export default class PageController {
           remove(this._showMoreButtonComponent);
         }
       };
+
+
+      // Функция рендеринга кнопки Show More
+      const renderShowMoreButton = () => {
+        if (showingFilmsCount >= films.length) {
+          return;
+        }
+
+        // Отрендерим кнопку
+        render(this._mainFilmsListComponent.getElement(), this._showMoreButtonComponent, RenderPosition.BEFOREEND);
+
+        // Добавим функционал для "Show more"
+        this._showMoreButtonComponent.setClickHandler(showMoreFilms);
+      };
+
+
+      // Найдём главный контейнер для фильмов
+      const mainFilmsContainerElement = this._mainFilmsListComponent.getElement().querySelector(`.films-list__container`);
+
+      // Повесим обработчик клика на сортировку
+      this._sortComponent.setChangeSortTypeHandler((sortType) => {
+        let sortedFilms = [];
+
+        switch (sortType) {
+          case SortType.BY_DATE:
+            sortedFilms = films.slice().sort((a, b) => b.year - a.year);
+            break;
+          case SortType.BY_RATING:
+            sortedFilms = films.slice().sort((a, b) => b.rating - a.rating);
+            break;
+          case SortType.DEFAULT:
+            sortedFilms = films.slice(0, showingFilmsCount);
+            break;
+        }
+
+        mainFilmsContainerElement.innerHTML = ``;
+
+        renderFilmCards(mainFilmsContainerElement, sortedFilms);
+
+        // Если сортировка была по дефолту, то отобразим кнопку. Нет - удалим
+        if (sortType === SortType.DEFAULT) {
+          renderShowMoreButton();
+        } else {
+          remove(this._showMoreButtonComponent);
+        }
+      });
+
+      // Отобразим несколько фильмов при старте
+      let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+      renderFilmCards(mainFilmsContainerElement, films.slice(0, showingFilmsCount));
+
+      // Отображаем основной список и кнопку загрузки
+      render(container, this._mainFilmsListComponent, RenderPosition.BEFOREEND);
+      renderShowMoreButton();
+
+
+      // Отобразим доп. секции с фильмами и сразу найдём в них контейнеры для фильмов
+      render(container, this._topRatedFilmsComponent, RenderPosition.BEFOREEND);
+      const topRatedFilmsContainerElement = this._topRatedFilmsComponent.getElement().querySelector(`.films-list__container`);
+
+      render(container, this._mostCommentedFilmsComponent, RenderPosition.BEFOREEND);
+      const mostCommentedFilmsContainerElement = this._mostCommentedFilmsComponent.getElement().querySelector(`.films-list__container`);
+
+
+      // Отрендерим фильмы в доп. секциях по параметрам
+      checkFilmsOnParam(
+          films,
+          FilmParametres.RATING,
+          topRatedFilmsContainerElement);
+
+      checkFilmsOnParam(
+          films,
+          FilmParametres.COMMENTS,
+          mostCommentedFilmsContainerElement);
 
       // Добавим функционал для "Show more"
       this._showMoreButtonComponent.setClickHandler(showMoreFilms);
