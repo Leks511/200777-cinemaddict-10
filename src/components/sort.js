ba7a9366f630064e2dd85ccbd605ddff5985ebda
Александр <leks511@bk.ru>
2020-01-07 14:36:29 +0300

    Дописал компонент Sort: добавлены дата-атрибуты

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,16 +1,28 @@
 import AbstractComponent from './abstract-component';
 
+export const SortType = {
+  DEFAULT: `default`,
+  BY_DATE: `date`,
+  BY_RATING: `rating`
+};
+
 const createSortTemplate = () => {
   return (
     `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" data-sort-type="${SortType.BY_DATE}" class="sort__button">Sort by date</a></li>
+      <li><a href="#" data-sort-type="${SortType.BY_RATING}" class="sort__button">Sort by rating</a></li>
     </ul>`
   );
 };
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortTemplate();
   }
