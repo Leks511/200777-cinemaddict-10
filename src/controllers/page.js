83ac50c1d2eaaaba220e6e365a109a1690901d3f
Leks <leks511@bk.ru>
2020-08-12 02:36:01 +0300

    Добавил функционал сотировки на страницу, вынес рендеринг кнопки в функцию

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -1,6 +1,6 @@
 import {RenderPosition, render, remove} from "../utils/render.js";
 
-import SortComponent from "../components/sort.js";
+import SortComponent, {SortType} from "../components/sort.js";
 import FilmsComponent from "../components/films.js";
 import AllMoviesComponent from "../components/all-movies.js";
 import TopRatedComponent from "../components/top-rated.js";
@@ -19,6 +19,26 @@ const FilmPatameters = {
 };
 
 const bodyElement = document.body;
+
+// Функция сортировки фильмов
+const getSortedFilms = (films, sortType, from, to) => {
+  let sortedFilms = [];
+  const showingFilms = films.slice();
+
+  switch (sortType) {
+    case SortType.DATE:
+      sortedFilms = showingFilms.sort((a, b) => a.releaseDate - b.releaseDate);
+      break;
+    case SortType.RATING:
+      sortedFilms = showingFilms.sort((a, b) => a.rating - b.rating);
+      break;
+    case SortType.DEFAULT:
+      sortedFilms = showingFilms;
+      break;
+  }
+
+  return sortedFilms.slice(from, to);
+};
 
 // Функция рендеринга фильма
 const renderFilm = (filmData, filmsList) => {
@@ -49,9 +69,8 @@ const renderFilm = (filmData, filmsList) => {
   render(filmsList, filmCardComponent, RenderPosition.BEFOREEND);
 };
 
-const renderFilms = (films, container, from, to) => {
+const renderFilms = (films, container) => {
   films
-    .slice(from, to)
     .forEach((film) => renderFilm(film, container));
 };
 
@@ -69,61 +88,97 @@ export default class PageController {
     this._showMoreButtonComponent = new ShowMoreButtonComponent();
   }
 
+  // Метод наполнения страницы контентом
   renderFilms(films) {
-
-    const container = this._container.getElement();
-    render(container, this._sortComponent, RenderPosition.BEFOREEND);
-    render(container, this._filmsComponent, RenderPosition.BEFOREEND);
-
-    const contentContainer = this._filmsComponent.getElement();
-
-    if (films.length) {
-      render(contentContainer, this._allMoviesComponent, RenderPosition.BEFOREEND);
-
-      const allMovies = this._allMoviesComponent.getElement();
-      const filmsContainer = allMovies.querySelector(`.films-list__container`);
-
-      // Функционал кнопки
-
-      let showingFilms = SHOWING_FILMS_COUNT_ON_START;
-
-      renderFilms(films, filmsContainer, 0, showingFilms);
-
-      if (films.length > SHOWING_FILMS_COUNT_BY_BUTTON) {
-        const showMoreButtonComponent = this._showMoreButtonComponent;
-
-        render(allMovies, showMoreButtonComponent, RenderPosition.BEFOREEND);
-
-        showMoreButtonComponent.setClickHandler(() => {
-          renderFilms(films, filmsContainer, showingFilms, showingFilms += SHOWING_FILMS_COUNT_BY_BUTTON);
-
-          if (showingFilms >= films.length) {
-            remove(showMoreButtonComponent);
-          }
-        });
+    // Функция рендеринга кнопки
+    const renderShowMoreButton = () => {
+      // Если число фильмов для показа больше или равно фильмов, то не кнопка не рендерится
+      if (showingFilmsCount >= films.length) {
+        return;
       }
 
+      // Рендерим кнопку
+      render(allMoviesSectionElement, this._showMoreButtonComponent, RenderPosition.BEFOREEND);
 
-      const renderExtra = (component, type) => {
-        const sortedFilms = films
-          .slice()
-          .filter((film) => film[type] > 0)
-          .sort((a, b) => b[type] - a[type]);
+      // Вешаем на неё обработчик
+      this._showMoreButtonComponent.setClickHandler(() => {
+        const prevFilmsCount = showingFilmsCount;
+        showingFilmsCount += SHOWING_FILMS_COUNT_BY_BUTTON;
 
-        if (sortedFilms.length) {
-          render(contentContainer, component, RenderPosition.BEFOREEND);
+        // Берём отсортированные фильмы по типу
+        const sortedFilms = getSortedFilms(films, this._sortComponent.getSortType(), prevFilmsCount, showingFilmsCount);
 
-          const extraContainer = component.getElement().querySelector(`.films-list__container`);
+        // Рендерим их в контейнер
+        renderFilms(sortedFilms, allMoviesListElement);
 
-          renderFilms(sortedFilms, extraContainer, 0, 2);
+        // Если по итогу кол-во фильмов для показа больше, чем общее кол-во или равно, то убираем кнопку
+        if (showingFilmsCount >= films.length) {
+          remove(this._showMoreButtonComponent);
         }
-      };
+      });
+    };
 
-      renderExtra(this._topRatedComponent, FilmPatameters.RATING);
-      renderExtra(this._mostCommentedComponent, FilmPatameters.COMMENTS_COUNT);
 
-    } else {
-      render(contentContainer, this._noDataComponent, RenderPosition.BEFOREEND);
+    // Найдём контейнер контроллера и наполним его компонентом сортировки и фильм-листа
+    const containerElement = this._container.getElement();
+    render(containerElement, this._sortComponent, RenderPosition.BEFOREEND);
+    render(containerElement, this._filmsComponent, RenderPosition.BEFOREEND);
+
+    // Найдём главную доску фильмов
+    const filmsElement = this._filmsComponent.getElement();
+
+    // Если фильмов нет, то скажем, что фильмов нет
+    if (!films.length) {
+      render(filmsElement, this._noDataComponent, RenderPosition.BEFOREEND);
+      return;
     }
+
+
+    // Иначе - отрендерим секцию для всех фильмов и найдём элемент контейнера
+    render(filmsElement, this._allMoviesComponent, RenderPosition.BEFOREEND);
+
+    const allMoviesSectionElement = this._allMoviesComponent.getElement();
+    const allMoviesListElement = allMoviesSectionElement.querySelector(`.films-list__container`);
+
+
+    let showingFilmsCount = SHOWING_FILMS_COUNT_ON_START;
+
+    // Сразу отрендерим первые имеющиеся фильмы и кнопку
+    renderFilms(films.slice(0, showingFilmsCount), allMoviesListElement);
+    renderShowMoreButton();
+
+    // Вызовем метод sortComponent для подписки на обработчик: повесим обработчик
+    this._sortComponent.setSortTypeHandler((sortType) => {
+      showingFilmsCount = SHOWING_FILMS_COUNT_BY_BUTTON;
+
+      const sortedFilms = getSortedFilms(films, sortType, 0, showingFilmsCount);
+
+      allMoviesListElement.innerHTML = ``;
+
+      renderFilms(sortedFilms, allMoviesListElement);
+      renderShowMoreButton();
+    });
+
+
+    // Функция для наполнения доп. секций
+    const renderExtra = (component, type) => {
+      const sortedFilms = films
+        .slice()
+        .filter((film) => film[type] > 0)
+        .sort((a, b) => b[type] - a[type]);
+
+      if (sortedFilms.length) {
+        render(filmsElement, component, RenderPosition.BEFOREEND);
+
+        const extraContainer = component.getElement().querySelector(`.films-list__container`);
+
+        renderFilms(sortedFilms.slice(0, 2), extraContainer);
+      }
+    };
+
+
+    // Наполним Top Rated и Most Commented
+    renderExtra(this._topRatedComponent, FilmPatameters.RATING);
+    renderExtra(this._mostCommentedComponent, FilmPatameters.COMMENTS_COUNT);
   }
 }
